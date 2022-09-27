e43a2942cc9165dc85e16bb0c712ce8834a84ad8
Peter Mumford <sunraydust@gmail.com>
2022-09-27 13:14:03 -0400

    map initializes with user location

--- src/components/HotspotMap.vue
+++ src/components/HotspotMap.vue
@@ -18,17 +18,35 @@ export default {
     mounted() {
         mapboxgl.accessToken =
             'pk.eyJ1IjoicGhvdG9jdXJpbyIsImEiOiJja3FqeDF5M2UwNHZ4MnZydXB2dXcyMzFoIn0.pwFXFrly8A-FTseV_kBlVg'
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/outdoors-v11',
-            center: [-71, 42.7], // starting position [lng, lat]
-            zoom: 9.5
-        })
-        map.on('load', () => {
-            this.applyHotspots(map)
-        })
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((e) => {
+                this.initMap([e.coords.longitude, e.coords.latitude])
+            })
+        } else {
+            this.initMap([-74.5, 40])
+        }
     },
     methods: {
+        initMap(location) {
+            window.map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/outdoors-v11',
+                center: location, // [lng, lat]
+                zoom: 9.5
+            })
+            map.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+                    trackUserLocation: false
+                })
+            )
+            map.on('load', () => {
+                this.applyHotspots(map)
+            })
+            map.on('move', () => console.log(map.getBounds()))
+        },
         async getHotspots() {
             const requestOptions = {
                 method: 'GET',
@@ -36,7 +54,7 @@ export default {
                     'Content-Type': 'application/json'
                 }
             }
-            const res = await fetch(`/.netlify/functions/hotspots?regioncode=US-MA-009&back=8`, requestOptions)
+            const res = await fetch(`/.netlify/functions/hotspots?regioncode=US-MA-017&back=7`, requestOptions)
             if (!res.ok) {
                 const message = `Error: ${res.status}, ${res.statusText}`
                 this.hotspots = [message]
@@ -45,12 +63,12 @@ export default {
         },
         applyHotspots(map) {
             this.hotspots.forEach((hotspot) => {
-                const el = document.createElement('div')
+                const el = document.createElement('button')
                 el.className = 'marker'
                 el.setAttribute('data-bs-toggle', 'popover')
                 el.setAttribute('data-bs-content', hotspot.properties.locName)
                 new mapboxgl.Marker(el).setLngLat(hotspot.geometry.coordinates).addTo(map)
-                new Popover(el, { trigger: 'hover' })
+                new Popover(el, { trigger: 'focus hover' })
             })
         }
     }
