2cca0e245143eba8c96f1f86e56248ce45f13ac3
Peter Mumford <sunraydust@gmail.com>
2022-09-27 21:47:35 -0400

    tweak footer text

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer mt-auto py-3 bg-light">
         <p class="text-center text-muted mb-0">
-            Made by <a href="https://petermumford.net/">Peter Mumford</a>, data from
+            Made by <a href="https://petermumford.net/">Peter Mumford</a> with data from
             <a href="https://ebird.org/home">ebird</a>.
         </p>
     </footer>
