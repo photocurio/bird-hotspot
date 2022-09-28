0e3ca8fe6ce21432d08a57132f0fa507c5919bf4
Peter Mumford <sunraydust@gmail.com>
2022-09-28 10:36:03 -0400

    cache hotspot data with Redis

--- functions/hotspots.js
+++ functions/hotspots.js
@@ -1,54 +1,75 @@
-// const { URL } = require('url')
+const { URL } = require('url')
 const fetch = require('node-fetch')
+const Redis = require('ioredis')
+
+// client.set('foo', 'bar');
 exports.handler = async (event) => {
 	const { regioncode, back } = event.queryStringParameters
-	const requestOptions = {
-		method: 'GET',
-		redirect: 'follow'
-	}
-
 	// @ts-ignore
-	const hotspots = await fetch(
-		`https://api.ebird.org/v2/ref/hotspot/${regioncode}?fmt=json&back=${back}`,
-		requestOptions
-	)
-		.then(response => {
-			if (response.status === 200) return response.json()
-			else return {
-				responseType: 'error',
-				statusCode: response.status,
-				body: response.statusText
-			}
-		})
-		.then(data => {
-			if (data.responseType === 'error') return data
-			const geoJason = data.map(spot => {
-				return {
-					geometry: {
-						type: 'Point',
-						coordinates: [
-							spot.lng,
-							spot.lat
-						]
-					},
-					type: 'Feature',
-					properties: {
-						locId: spot.locId,
-						locName: spot.locName,
-						countryCode: spot.countryCode,
-						subnational1Code: spot.subnational1Code,
-						subnational2Code: spot.subnational2Code
-					}
-				}
-			})
-			return geoJason
-		})
-		.catch(error => {
-			return error
-		})
+	const redisClient = new Redis("rediss://:bf0ef24c62d848ddbbe14c5cc3ee68b7@us1-big-albacore-38516.upstash.io:38516")
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify(hotspots)
+	const requestName = `county-hotspots-${regioncode}`
+	try {
+		const cachedRes = await redisClient.get(requestName)
+		if (cachedRes) {
+			console.log('got it from cache')
+			return {
+				statusCode: 200,
+				body: JSON.stringify(JSON.parse(cachedRes))
+			}
+		} else {
+			const ebirdApi = new URL(`https://api.ebird.org/v2/ref/hotspot/${regioncode}`)
+			ebirdApi.searchParams.set('fmt', 'json')
+			ebirdApi.searchParams.set('back', back)
+			// @ts-ignore
+			const hotspots = await fetch(ebirdApi, { method: 'GET', redirect: 'follow' })
+				.then(response => {
+					if (response.status === 200) return response.json()
+					else return {
+						responseType: 'error',
+						statusCode: response.status,
+						body: response.statusText
+					}
+				})
+				.then(data => {
+					if (data.responseType === 'error') return data
+					const geoJason = data.map(spot => {
+						return {
+							geometry: {
+								type: 'Point',
+								coordinates: [
+									spot.lng,
+									spot.lat
+								]
+							},
+							type: 'Feature',
+							properties: {
+								locId: spot.locId,
+								locName: spot.locName,
+								countryCode: spot.countryCode,
+								subnational1Code: spot.subnational1Code,
+								subnational2Code: spot.subnational2Code
+							}
+						}
+					})
+					return geoJason
+				})
+				.catch(error => {
+					return error
+				})
+			console.log('got it from ebird, setting cache now	')
+			// cache suration 1 day (86400 seconds)
+			redisClient.set(requestName, JSON.stringify(hotspots), 'EX', 86400)
+			return {
+				statusCode: 200,
+				body: JSON.stringify(hotspots)
+			}
+		}
+	}
+	catch (err) {
+		return {
+			statusCode: 500,
+			body: 'Something went wrong while fetching the hotspots.'
+		}
 	}
 }
