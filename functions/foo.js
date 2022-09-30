2893072c61a804bd7737fb0aabcb2beedc14ff74
Peter Mumford <sunraydust@gmail.com>
2022-09-30 08:16:15 -0400

    try catch in foo

--- functions/foo.js
+++ functions/foo.js
@@ -1,11 +1,20 @@
 const { Redis } = require('@upstash/redis')
 
 exports.handler = async () => {
-	const redis = Redis.fromEnv()
-	await redis.set('foo', 'bar')
-	const bar = await redis.get('foo')
-	return {
-		statusCode: 200,
-		body: JSON.stringify(bar)
+	try {
+		const redis = Redis.fromEnv()
+		await redis.set('foo', 'bar')
+		const bar = await redis.get('foo')
+		return {
+			statusCode: 200,
+			body: JSON.stringify(bar)
+		}
+	}
+	catch (error) {
+		console.log(error)
+		return {
+			statusCode: error.statusCode,
+			body: 'this sucked'
+		}
 	}
 }
