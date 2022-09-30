e47a1cc689e8af760648b77312df57d0a9b9ea70
Peter Mumford <sunraydust@gmail.com>
2022-09-30 13:18:22 -0400

    added redis to observations

--- functions/observations.js
+++ functions/observations.js
@@ -2,30 +2,59 @@
 const fetch = require('node-fetch')
 exports.handler = async (event) => {
 	const { locationCode, back } = event.queryStringParameters
-	const requestOptions = {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-eBirdApiToken': process.env.EBIRD_TOKEN
-		}
-	}
-
+	const requestName = `observations-${locationCode}`
 	// @ts-ignore
-	const hotspots = await fetch(
-		`https://api.ebird.org/v2/data/obs/${locationCode}/recent?fmt=json&back=${back}`,
-		requestOptions
+	const cachedRes = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/${requestName}`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+			}
+		}
 	)
-		.then(response => {
-			return response.status === 200 ?
-				response.json() :
-				`error code: ${response.status}`
-		})
-		.catch(error => {
-			return error
-		})
-
-	return {
-		statusCode: 200,
-		body: JSON.stringify(hotspots)
+	const jsonRes = await cachedRes.json()
+	if (jsonRes.result) {
+		const parsedRes = JSON.parse(jsonRes.result)
+		return {
+			statusCode: 200,
+			body: JSON.stringify(parsedRes)
+		}
+	} else {
+		// @ts-ignore
+		const obs = await fetch(
+			`https://api.ebird.org/v2/data/obs/${locationCode}/recent?fmt=json&back=${back}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-eBirdApiToken': process.env.EBIRD_TOKEN
+				}
+			}
+		)
+			.then(response => {
+				return response.status === 200 ?
+					response.json() :
+					`error code: ${response.status}`
+			})
+			.catch(error => {
+				return error
+			})
+		const obsString = JSON.stringify(obs)
+		// @ts-ignore
+		const setRedis = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/set/${requestName}/?EX=85400`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+				},
+				body: obsString
+			}
+		)
+		return {
+			statusCode: 200,
+			body: obsString
+		}
 	}
 }
