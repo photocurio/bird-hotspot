7fa74f5a1977e1b8d604ff0fdc5cb093e9abea71
Peter Mumford <sunraydust@gmail.com>
2022-10-06 07:16:45 -0400

    changed about and footer text

--- src/components/AboutModal.vue
+++ src/components/AboutModal.vue
@@ -7,13 +7,7 @@
         </p>
         <p>
             To navigate to a town or city, enter your location and a state, in the search form:
-            <code>Missoula, MT</code>.
+            <code>Missoula, MT</code>. It only works in the United States.
         </p>
-        <p>
-            This app was developed by <a href="https://petermumford.net/">Peter Mumford</a>, with data from
-            <a href="https://ebird.org/home">eBird</a>.
-        </p>
-        <p>It only works in the United States, unfortunately.</p>
-        <p>Flying bird icon courtesy <a href="https://www.fredsprinkle.com/">Fred Sprinkle</a>.</p>
     </div>
 </template>
