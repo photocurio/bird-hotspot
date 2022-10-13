f8a855eafe1f7f9652ba3141a6794e4e91326576
Peter Mumford <sunraydust@gmail.com>
2022-10-13 16:32:27 -0400

    rewrote MapView
    Getting user location returns promise, if location is denied, one of 10 default locations is loaded.

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,24 +1,73 @@
-import React, { useState } from 'react'
-import Map from 'react-map-gl'
+import React, { useState, useEffect } from 'react'
+import defaultLocations from '../data/defaultLocations'
+import Map, { Layer, Source, NavigationControl } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
 const mapboxApiKey = 'pk.eyJ1IjoicGhvdG9jdXJpbyIsImEiOiJja3FqeDF5M2UwNHZ4MnZydXB2dXcyMzFoIn0.pwFXFrly8A-FTseV_kBlVg'
 
-function MapView() {
-	const [viewState, setViewState] = useState({
-		longitude: -73.58781,
-		latitude: 45.50884,
-		zoom: 7
+function getCoords() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+			pos => resolve({
+				longitude: pos.coords.longitude,
+				latitude: pos.coords.latitude,
+				zoom: 9.5
+			}),
+			err => reject(err)
+		)
 	})
+}
 
-	return (
+function MapView() {
+	// choose 1 of ten default locations
+	const int = Math.floor(Math.random() * 10)
+
+	const [viewState, setViewState] = useState(null)
+
+	async function getPosition() {
+		try {
+			const coords = await getCoords()
+			setViewState(coords)
+		}
+		catch (err) {
+			console.log(err.message)
+			setViewState({
+				longitude: defaultLocations[int][0],
+				latitude: defaultLocations[int][1],
+				zoom: 9.5
+			})
+		}
+	}
+
+	useEffect(() => {
+		getPosition()
+	}, [])
+
+	const countyLayer = {
+		id: 'counties',
+		type: 'fill',
+		source: 'counties',
+		'source-layer': 'original',
+		paint: {
+			'fill-outline-color': 'rgba(0,0,0,0.5)',
+			'fill-color': 'rgba(0,0,0,0)'
+		}
+	}
+	// early return is viewState has no coordinates
+	if (!viewState) return
+
+	else return (
 		<Map
 			{...viewState}
 			mapboxAccessToken={mapboxApiKey}
 			mapStyle="mapbox://styles/mapbox/outdoors-v11"
 			onMove={evt => setViewState(evt.viewState)}
 		>
+			<NavigationControl />
+			<Source type="vector" url="mapbox://mapbox.82pkq93d">
+				<Layer {...countyLayer} />
+			</Source>
 		</Map>
 	)
 }
