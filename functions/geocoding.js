2579dd23537ba63a09baf0533a84778d79c1cbb4
Peter Mumford <sunraydust@gmail.com>
2022-10-18 15:48:19 -0400

    formatting

--- functions/geocoding.js
+++ functions/geocoding.js
@@ -1,17 +1,17 @@
-const fetch = require('node-fetch')
+const fetch = require( 'node-fetch' )
 
-exports.handler = async (event) => {
+exports.handler = async ( event ) => {
 	const { q } = event.queryStringParameters
 	const username = process.env.GEONAMES_USERNAME
 	const geocodeUrl = `http://api.geonames.org/geoCodeAddressJSON?q=${q}&username=${username}`
 
 	// @ts-ignore
-	const res = await fetch(geocodeUrl)
-	if (res.status === 200) {
+	const res = await fetch( geocodeUrl )
+	if ( res.status === 200 ) {
 		const resJson = await res.json()
 		return {
 			statusCode: 200,
-			body: JSON.stringify(resJson)
+			body: JSON.stringify( resJson )
 		}
 	} else return {
 		responseType: 'error',
