67a68d76bd2b0c4db2fa16888f9a3a77b012bddd
Peter Mumford <sunraydust@gmail.com>
2023-01-24 10:38:07 -0500

    Reformatting

--- src/data/defaultLocations.js
+++ src/data/defaultLocations.js
@@ -1,12 +1,12 @@
 export default [
-    [-71.4112246, 41.8167968], 	// Providence, RI
-    [-73.442951, 43.8372667], 	// Ticonderoga, NY
-    [-90.0226488, 30.0329222], 	// New Orleans, LA
-    [-97.7317195, 30.0329222], 	// Austin, TX
-    [-91.6578787, 41.9650539], 	// Cedar Rapids, IA
-    [-117.6301623, 33.6279334],	// Rancho Santa Margarita, CA
-    [-105.1786789, 40.0134537],	// Boulder, CO
-    [-113.8505913, 37.1044878],	// St George, UT
-    [-120.6563104, 35.272491], 	// San Luis Obispo, CA
-    [-122.3310856, 47.6129432] 	// Seattle, WA
+	[-71.4112246, 41.8167968], 	// Providence, RI
+	[-73.442951, 43.8372667], 	// Ticonderoga, NY
+	[-90.0226488, 30.0329222], 	// New Orleans, LA
+	[-97.7317195, 30.0329222], 	// Austin, TX
+	[-91.6578787, 41.9650539], 	// Cedar Rapids, IA
+	[-117.6301623, 33.6279334],	// Rancho Santa Margarita, CA
+	[-105.1786789, 40.0134537],	// Boulder, CO
+	[-113.8505913, 37.1044878],	// St George, UT
+	[-120.6563104, 35.272491], 	// San Luis Obispo, CA
+	[-122.3310856, 47.6129432] 	// Seattle, WA
 ]
